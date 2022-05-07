134ff88b1277eece2829a05a30e0ec00b7f6b1d6
Ron Skufca <ron.skufca@ngic.com>
2022-05-07 15:15:08 -0400

    updated comments

--- src/LeetCode/TwoSumProblem.cs
+++ src/LeetCode/TwoSumProblem.cs
@@ -12,9 +12,15 @@ namespace LeetCode.TwoSumProblem
 			// 2+4=6
 			// 5+1=6
 			// The brute force way would be to have 2 loops, but this is bad as it is O(n^2)
-			int[] arr = { 1, 2, 3, 4, 5 };			
+			int[] arr = { 2, 7, 11, 15 };
 
-			new TwoSumProblem().CalculateTwoSum(arr, 6);
+			var twoSum = new TwoSumProblem();
+			// output should be [0,1]
+			//twoSum.CalculateTwoSum(arr, 9);
+
+			int[] arr1 = {3, 2, 4};
+			// output should be [1,2]
+			twoSum.CalculateTwoSumForSortedArray(arr1, 6);
 		}
 
 		private void PrintResult(int[] nums)
@@ -29,44 +35,51 @@ namespace LeetCode.TwoSumProblem
 	{
 		/// <summary>
 		/// Figure out the pair of elementes where arr[p] + arr[q] add up to a certain number
+		/// THIS WILL ONLY WORK WHEN ARR IS SORTED IN ASCENDING ORDER
 		/// </summary>
 		/// <param name="arr"></param>
-		public void CalculateTwoSum(int[] arr, int targetValue)
+		public int[] CalculateTwoSumForSortedArray(int[] arr, int targetValue)
 		{
 			// First index in the array
-			int pointer_one = 0;
+			int pointer_start = 0;
 
 			// Last possible index in the array
-			int pointer_two = arr.Length - 1; 
+			int pointer_end = arr.Length - 1;
+
+			// indexes for the 2 items which sum to the targetValue
+			int[] targetIndexes = new int[2];
 
 			// Assumptions
 			// 1. The array is sorted
 			// 2. Add the values of pointer_one and pointer_two
-			while(pointer_one < pointer_two)
+			while(pointer_start < pointer_end)
 			{
 				// Add the values of pointer_one and pointer_two
-				int currentSum = arr[pointer_one] + arr[pointer_two];
+				int currentSum = arr[pointer_start] + arr[pointer_end];
 
 				// If the sum matches our target value. YEA WE ARE DONE.
 				if(currentSum == targetValue)
 				{
-					Console.WriteLine($"Element {arr[pointer_one]} + {arr[pointer_two]} does equal our target value of {targetValue}");
-					pointer_one++;
-					pointer_two--;
+					Console.WriteLine($"Element {arr[pointer_start]} + {arr[pointer_end]} does equal our target value of {targetValue}");
+					targetIndexes[0] = pointer_start;
+					targetIndexes[1] = pointer_end;
+					pointer_start++;
+					pointer_end--;
 				}
 				// If the sum is less than the target value
 				// we increment pointer_one. Move your left pointer 1 place to the right
 				else if(currentSum < targetValue)
 				{
-					pointer_one++;					
+					pointer_start++;					
 				}
 				// If the sum is greater than the target value
 				// we decrement pointer_two. Move your right pointer 1 place to the left
 				else if(currentSum > targetValue)
 				{
-					pointer_two--;
-				}				
+					pointer_end--;
+				}			
 			}
+			return targetIndexes;
 		}
 	}
 }
