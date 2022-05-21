9d776c8b99070c22a9663e195aa523c6b6ff825c
Ron Skufca <ron.skufca@ngic.com>
2022-05-21 14:40:38 -0400

    created a binary search tree for questions

---
+++ DataStructures/BinarySearchTree.cs
@@ -0,0 +1,85 @@
+﻿namespace DataStructures.BinarySearchTree
+{
+	public class Node
+	{
+		public string Question { get; set; }
+
+		/// <summary>
+		/// Left side is defined as a yes
+		/// </summary>
+		public Node Yes { get; set; } = null;
+
+		/// <summary>
+		/// Right side is defined as a no
+		/// </summary>
+		public Node No { get; set; } = null;
+
+		public Node(string question)
+		{
+			Question = question;
+		}
+	}
+
+	public class Questions
+	{
+		private Node _root { get; set; } = null;
+
+		public Questions()
+		{
+
+		}
+
+		public void Insert(string question, QuestionResponse questionResponse)
+		{
+			Node currentNode = _root;
+
+			Node newNode = new Node(question);
+
+			// we are inserting the root or first node
+			if(currentNode == null)
+			{
+				_root = newNode;
+				return;
+			}
+
+			if(newNode.Question == currentNode.Question)
+				return;
+
+			// Yes responses will go on the left side of the node
+			if(questionResponse == QuestionResponse.YES)
+			{
+				if(currentNode.Yes == null)
+				{
+					currentNode.Yes = newNode;
+				}
+				else
+				{
+					currentNode = currentNode.Yes;
+					Insert(newNode.Question, questionResponse);
+				}
+			}
+			else if(questionResponse == QuestionResponse.NO)
+			{
+				if(currentNode.No == null)
+				{
+					currentNode.No = newNode;
+				}
+				else
+				{
+					currentNode = currentNode.No;
+					Insert(newNode.Question, questionResponse);
+				}
+			}			
+		}		
+	}
+
+	public enum QuestionResponse
+	{
+		// Response was yes
+		YES,
+		// Response was no
+		NO,
+		// No response necessary
+		NoResponseNecessary
+	}
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,25 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using DataStructures;
+using DataStructures.BinarySearchTree;
 
-var myList = new LinkedList();
+var questionTree = new Questions();
+questionTree.Insert("During the last three years have you lived outside of Oregon for 60 or more consecutive days?", QuestionResponse.NoResponseNecessary);
+questionTree.Insert("Please list the last three locations. Please include the city, state, and residence dates for all locations.", QuestionResponse.YES);
+questionTree.Insert("Most recent city", QuestionResponse.NoResponseNecessary);
 
-myList.Insert("Ron");
-myList.Insert("Skufca");
-myList.Insert("Is");
-myList.Insert("the");
-myList.Insert("greatest");
-myList.Insert("mind");
-myList.Insert("in");
-myList.Insert("the");
-myList.Insert("universe");
-myList.Insert(".");
 
-myList.TryDelete("Ron");
-myList.PrintList();
-Console.WriteLine();
+//var myList = new LinkedList();
+//myList.Insert("Ron");
+//myList.Insert("Skufca");
+//myList.Insert("Is");
+//myList.Insert("the");
+//myList.Insert("greatest");
+//myList.Insert("mind");
+//myList.Insert("in");
+//myList.Insert("the");
+//myList.Insert("universe");
+//myList.Insert(".");
 
-myList.TryDelete("the");
-myList.PrintList();
+//myList.TryDelete("Ron");
+//myList.PrintList();
+//Console.WriteLine();
+
+//myList.TryDelete("the");
+//myList.PrintList();
 
 //var nodeFound = myList.TryFind("in", out Node foundNode);
 //var nodeValue = (foundNode.Value != null) ? foundNode.Value : "not found";
