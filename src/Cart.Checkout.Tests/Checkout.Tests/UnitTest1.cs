31c069c7f1e5f74b4eea31a1ce121d850f81f983
Ron Skufca <ron.skufca@gmail.com>
2022-11-19 14:56:26 -0500

    Added my shopping cart kata

---
+++ src/Cart.Checkout.Tests/Checkout.Tests/UnitTest1.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+using System.Collections.ObjectModel;
+
+namespace ShopingCart.Tests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        /// <summary>
+        /// Scenario: Checkout accepts n number of items
+        ///    Given: 3 number of items scanned in
+        ///      And: Clerk requests a price check
+        ///     Then: We return the total of the 3 items which is 100
+        /// </summary>
+        [TestMethod]
+        public void Checkout_Accepts_Items()
+        {
+            var checkout = new Checkout();
+
+            var items = new Collection<char>() { 'A', 'B', 'C' };
+
+            var totalPrice = checkout.GetTotalPrice(items);
+
+            Assert.AreEqual(totalPrice, 100);
+        }
+
+        /// <summary>
+        /// Scenario: Checkout accepts 0 items which then cost nothing
+        ///    Given: 0 number of items scanned in
+        ///      And: Clerk requests a price check
+        ///     Then: We return 0 cost
+        /// </summary>
+        [TestMethod]
+        public void Checkout_Rejects_No_Cost()
+        {
+
+            var checkout = new Checkout();
+
+            var items = new Collection<char>() { };
+
+            var totalPrice = checkout.GetTotalPrice(items);
+
+            Assert.AreEqual(0, totalPrice);
+        }
+
+        [TestMethod]
+        public void Checkout_Calculates_Special_Offer_Total()
+        {
+            var checkout = new Checkout();
+
+            var items = new Collection<char>() { 'A', 'A', 'A' };
+
+            var totalPrice = checkout.GetTotalPrice(items);
+
+            Assert.AreEqual(130, totalPrice);
+        }
+    }
+}
---
+++ src/Cart.Checkout/Checkout.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ShoppingCart
+{
+    public sealed class Checkout
+    {
+        private const int A = 50;
+        private const int B = 30;
+        private const int C = 20;
+
+        private ICollection<Offer> _offers;
+
+        public Checkout()
+        {
+            _offers = new Collection<Offer>()
+            {
+                new Offer('A', 3, 130)
+            };
+        }
+
+        public int GetTotalPrice(ICollection<char> items)
+        {
+            if (!items.Any())
+                return 0;
+
+            var totalPrice = 0;
+
+            if(EligibleForSpecialOffer(items))
+                
+
+            items.ToList().ForEach(item =>
+            {
+                switch (item)
+                {
+                    case 'A':
+                        if (EligibleForSpecialOffer(items))
+                        {
+                            totalPrice += _offers.FirstOrDefault(offer => offer.Item == 'A').Price;
+                        }
+                        else
+                        {
+                            totalPrice += A;
+                        }                            
+                        break;
+                    case 'B':
+                        totalPrice += B;
+                        break;
+                    case 'C':
+                        totalPrice += C;
+                        break;
+                    default:
+                        totalPrice += 0;
+                        break;
+
+                }
+            });
+
+            return totalPrice;
+        }
+
+        private bool EligibleForSpecialOffer(ICollection<char> items)
+        {
+            if (!items.Any())
+                return false;
+
+            // TODO: Need to find 3 of the same items in the collection which match any item in the Offers collection
+            var offersFound = items.Where(item => item.Equals('A'));
+
+            if(offersFound.Count() == 3)
+                return true;
+
+            return false;
+        }
+
+        public sealed class Offer
+        {
+            private char _item;
+            private int _quantity;
+            private int _price;
+
+            public char Item => _item;
+
+            public int Quantity => _quantity;
+
+            public int Price => _price;
+
+            public Offer(char item, int quantity, int offerPrice)
+            {
+                _item = item;
+                _quantity = quantity;
+                _price = offerPrice;
+            }
+        }
+    }
+}
