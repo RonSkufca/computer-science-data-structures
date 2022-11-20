bcf043fa911b0c7af6ec04c6ea6d84e82b8340df
Ron Skufca <ron.skufca@ngic.com>
2022-11-20 16:59:35 -0500

    have discounts working

---
+++ src/Cart.Checkout.Tests/Checkout.Tests/ItemDiscounterTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart.Tests
+{
+	[TestClass]
+	public sealed class ItemDiscounterTests
+	{
+		/// <summary>
+		/// Scenario: Apply disounts to total
+		///    Given: We have the following items 'a', 'b', 'a', 'c', 'a', 'a', 'c', 'd'
+		///      And: Items a and c are eligible for discounts
+		///      And: We call the item discounter to calculate the total discount
+		///     Then: The total discount should be 165
+		/// </summary>
+		[TestMethod]
+		public void Calculate_Discount_Returns_Total_Discount()
+		{
+			var specialOffers = new Collection<Offer>()
+			{
+				new Offer('A', 3, 130),
+				new Offer('C', 2, 35)
+			};
+
+			char[] items = new char[] {'A', 'B', 'A', 'C', 'A', 'A', 'C', 'D'};
+
+			var grouper = new ItemDiscounter(specialOffers);
+
+			var priceWithDiscountsApplied = grouper.GetTotalDiscount(items);
+
+			Assert.AreEqual(165, priceWithDiscountsApplied);
+		}
+	}
+}
---
+++ src/Cart.Checkout/ItemDiscounter.cs
@@ -0,0 +1,58 @@
+﻿namespace ShoppingCart
+{
+	public sealed class ItemDiscounter
+	{
+		private ICollection<Offer> _specialOffers;
+
+		public ItemDiscounter(ICollection<Offer> specialOffers)
+		{
+			_specialOffers = specialOffers;
+		}
+
+		public bool DiscountsExist(char[] items)
+		{
+			var itemGroup = from item in items
+							group item by item into g
+							select (item:g, qty:g.Count());
+
+			var itemList = itemGroup.Select(item => item.item.Key);
+
+			var itemsWithOffers = itemList.Intersect(_specialOffers.Select(item => item.Item));
+
+			return itemsWithOffers.Any();
+		}
+
+		public int GetTotalDiscount(char[] items)
+		{
+			IEnumerable<char> itemsWithOffers = BuildListOfDiscountedItems(items);
+
+			return CalculateDiscount(itemsWithOffers);
+		}
+
+		private int CalculateDiscount(IEnumerable<char> itemsWithOffers)
+		{
+			if (!itemsWithOffers.Any())
+				return 0;
+
+			int runningTotal = 0;
+
+			itemsWithOffers.ToList().ForEach(item =>
+			{
+				runningTotal += _specialOffers.Where(offer => offer.Item == item).FirstOrDefault().Price;
+			});
+
+			return runningTotal;
+		}
+
+		private IEnumerable<char> BuildListOfDiscountedItems(char[] items)
+		{
+			var itemGroup = from item in items
+							group item by item into g
+							select (item:g, qty:g.Count());
+
+			var itemList = itemGroup.Select(item => item.item.Key);
+
+			return itemList.Intersect(_specialOffers.Select(item => item.Item));
+		}
+	}
+}
